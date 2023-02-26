d937df428ddc44470d7f0918c27fcba18454b9bb
Onur Aktaş <aktsonr1903@gmail.com>
2023-02-26 21:16:54 +0300

    create vehicle-manager service

---
+++ vehicle-manager/src/test/java/com/onuraktas/vehiclemanager/VehicleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onuraktas.vehiclemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
