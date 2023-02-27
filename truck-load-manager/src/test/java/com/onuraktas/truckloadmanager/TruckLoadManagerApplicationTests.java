8606418dba9b7af6c05057d31a6e8389d5d9a1a7
Onur Aktaş <aktsonr1903@gmail.com>
2023-02-27 15:15:27 +0300

    create truck-load-manager service

---
+++ truck-load-manager/src/test/java/com/onuraktas/truckloadmanager/TruckLoadManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onuraktas.truckloadmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckLoadManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
