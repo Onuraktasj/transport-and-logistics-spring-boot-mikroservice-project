9551c5d20e0393bb917424c0db9ebd6c0bdcc918
Oguzcan BICER <oguzcanbicer96@gmail.com>
2023-03-01 20:46:27 +0300

    create driver-service-manager

---
+++ driver-service-manager/src/main/java/com/onuraktas/driverservicemanager/DriverServiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.onuraktas.driverservicemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverServiceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverServiceManagerApplication.class, args);
+    }
+
+}
